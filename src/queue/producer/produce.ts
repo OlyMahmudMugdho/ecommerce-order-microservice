735fb35253578c29d4e6bea214a3a718af9f645d
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 23:01:45 +0600

    added type safety

--- src/queue/producer/produce.ts
+++ src/queue/producer/produce.ts
@@ -1,12 +1,13 @@
+import { Order } from "../../types/types";
 import { producer } from "./producerConfig"
 
 
-export const produce = async (topicName: string, message: any) => {
+export const produce = async (topicName: string, message: Order) => {
     await producer.connect();
     await producer.send({
         topic: topicName,
         messages: [
-            message
+            { key: message.userId, value: message.toString() }
         ],
     });
     await producer.disconnect();
