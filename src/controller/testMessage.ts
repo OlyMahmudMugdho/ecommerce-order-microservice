88cb4e41b49641787dda6c56431dc6d00faedcb4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 16:58:52 +0600

    test message controller

---
+++ src/controller/testMessage.ts
@@ -0,0 +1,10 @@
+import { Hello } from "../types/types"
+import { Request, Response } from "express"
+
+const showMessage : object = (req : Request, res : Response) => {
+    const message : Hello = {
+        ok : true,
+        message : "controller test done"
+    }
+    res.status(200).json(message)
+}
