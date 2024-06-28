538d4c222ed061d8a99d34e1e814a944a9b14eb3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 22:34:11 +0600

    added type Producer

--- src/queue/producer/producerConfig.ts
+++ src/queue/producer/producerConfig.ts
@@ -1,8 +1,8 @@
-import { Partitioners } from "kafkajs";
+import { Partitioners, Producer } from "kafkajs";
 import { kafka } from "../client/kafkaClientConfig";
 
 
-export const producer = kafka.producer({
+export const producer: Producer = kafka.producer({
     createPartitioner: Partitioners.DefaultPartitioner,
     allowAutoTopicCreation: false,
 })
