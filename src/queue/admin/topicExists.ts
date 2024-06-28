59f6f71df3c5878dd70f98870fc677e28d462a84
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 18:13:05 +0600

    topic checker

---
+++ src/queue/admin/topicExists.ts
@@ -0,0 +1,12 @@
+import { getTopics } from "./getTopics"
+
+export const topicExists = async (topicName: string) => {
+    const topics = await getTopics();
+    const topic = topics.map(topic => topics.includes(topic))
+    if (topic.length == 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
