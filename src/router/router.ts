45dfb5cac3259a5d096ff88bd2150bf5f7ae2555
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 23:10:11 +0600

    added produce controller to route

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { showMessage } from "../controller/testMessage";
+import { produceOrder } from "../controller/produceOrder";
 
 export const router: Router = Router()
 
-router.get('/test', showMessage)
+router
+    .get('/test', showMessage)
+    .post('/order', produceOrder)
