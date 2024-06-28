64854afddcbed52a56f1ffd4b64a8fb9f5613fcc
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 13:06:03 +0600

    basic API

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,18 @@ app.use(express.json())
 
 const PORT: string | number = process.env.PORT || 8080;
 
+type Hello = {
+    ok: boolean;
+    message: string;
+}
+
+app.get("/", (req: Request, res: Response) => {
+    const message: Hello = {
+        ok: true,
+        message: "hello world"
+    }
+    res.status(200).json(message)
+})
 
 
 app.listen(8080, () => {
