b584b0b19663fdae13b7ff090ddcfc82edcf5927
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 13:07:03 +0600

    replaced or operator with '??'

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json())
 
-const PORT: string | number = process.env.PORT || 8080;
+const PORT: string | number = process.env.PORT ?? 8080;
 
 type Hello = {
     ok: boolean;
