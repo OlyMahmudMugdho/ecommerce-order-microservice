254a27a7e59f98019cc74022cb2f720ef0b0db03
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-28 21:44:07 +0600

    error handling

--- src/queue/admin/createTopic.ts
+++ src/queue/admin/createTopic.ts
@@ -1,21 +1,26 @@
 import { admin } from "./admin"
 
-export const createTopic = async (topicName : string) => {
-    await admin.connect();
-    const created = await admin.createTopics({
-        topics: [
-            {
-                topic: topicName,
-                numPartitions: 1,
-                replicationFactor: 1
-            }
-        ]
-    });
-    if (await created) {
-        console.log(`topic ${topicName} created successfully`)
-    }
-    else {
-        console.log(`topic ${topicName} creation failed`)
+export const createTopic = async (topicName: string) => {
+
+    try {
+        await admin.connect();
+        const created = await admin.createTopics({
+            topics: [
+                {
+                    topic: topicName,
+                    numPartitions: 1,
+                    replicationFactor: 1
+                }
+            ]
+        });
+        if (await created) {
+            console.log(`topic ${topicName} created successfully`)
+        }
+        else {
+            console.log(`topic ${topicName} creation failed`)
+        }
+    } catch (error) {
+        console.error(error)
     }
     await admin.disconnect();
 }
