d619ea99092395ac411d78970daf7a57a260e6d3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-29 11:36:40 +0600

    log level error

--- src/queue/client/kafkaClientConfig.ts
+++ src/queue/client/kafkaClientConfig.ts
@@ -1,6 +1,7 @@
-import { Kafka } from 'kafkajs'
+import { Kafka, logLevel } from 'kafkajs'
 
 export const kafka: Kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['localhost:9092']
+    brokers: ['localhost:9092'],
+    logLevel: logLevel.ERROR
 })
