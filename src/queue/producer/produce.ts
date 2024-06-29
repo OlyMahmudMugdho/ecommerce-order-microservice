7345a83a1c0d25973f449f86612f611580e5a9cf
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-29 11:21:46 +0600

    wrapped in try-catch

--- src/queue/producer/produce.ts
+++ src/queue/producer/produce.ts
@@ -3,12 +3,16 @@ import { producer } from "./producerConfig"
 
 
 export const produce = async (topicName: string, message: any) => {
-    await producer.connect();
-    await producer.send({
-        topic: topicName,
-        messages: [
-            { key: message.userId, value: message.toString() }
-        ],
-    });
-    await producer.disconnect();
+    try {
+        await producer.connect();
+        await producer.send({
+            topic: topicName,
+            messages: [
+                { key: message.userId, value: message.toString() }
+            ],
+        });
+        await producer.disconnect();
+    } catch (error) {
+        console.error(error)
+    }
 }
