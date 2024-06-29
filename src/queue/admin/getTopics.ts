5dad1cafb2e8d1b7917f47991af2f42255232d0b
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-29 11:24:04 +0600

    wrapped in try-catch

--- src/queue/admin/getTopics.ts
+++ src/queue/admin/getTopics.ts
@@ -1,8 +1,13 @@
 import { admin } from "./admin"
 
 export const getTopics = async () => {
-    await admin.connect();
-    const topics = await admin.listTopics();
-    await admin.disconnect();
-    return topics;
+    try {
+        await admin.connect();
+        const topics = await admin.listTopics();
+        await admin.disconnect();
+        return topics;
+    } catch (error) {
+        console.log(error)
+        return []
+    }
 }
