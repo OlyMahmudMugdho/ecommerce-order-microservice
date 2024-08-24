1950d4fef85e4d6e6ee41aa3d9faffdc9c46e52d
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-24 18:47:25 +0600

    kafka host change

--- src/queue/client/kafkaClientConfig.ts
+++ src/queue/client/kafkaClientConfig.ts
@@ -2,6 +2,6 @@ import { Kafka, logLevel } from 'kafkajs'
 
 export const kafka: Kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['localhost:9092'],
+    brokers: ['kafka:9092'],
     logLevel: logLevel.ERROR
 })
